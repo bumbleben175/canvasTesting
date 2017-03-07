f7dce57a373967bacedb7612f8ea841e012aa2d9
bumbleben175 <bumbleben175@gmail.com>
2017-03-07 07:22:51 -0800

    Add files via upload

---
+++ canvasTesting.js
@@ -0,0 +1,73 @@
+var tiles = 25;
+var startGame = function() {
+	myGameArea.start();
+	tile(tiles,tiles);
+	bobby = new makePlayer(tiles,tiles,"red",3,4);
+}
+
+
+//tiles the whole screen with "width" by "height" #tiles
+var tile = function(width,height) {
+	for (var h = 0; h <= height; h++) {
+		for (var w = 0; w <= width; w++) {
+			tileW = myGameArea.canvas.width/width;
+			tileH = myGameArea.canvas.height/height;
+			ctx = myGameArea.context;
+		    ctx.strokeStyle = "black";
+		    ctx.strokeRect(tileW*w, tileH*h, tileW, tileH);
+		}
+	}
+}
+
+
+//object constructor for making basic movable sprites
+var makePlayer = function(width,height,color,x,y){
+	//constant variables
+	this.width = width;
+    this.height = height;
+    //position variables
+    this.x = x;
+    this.y = y;
+    //changing position
+    this.speedX = 0;
+    this.speedY = 0;
+    this.controls = {
+    	moveUp: function(){
+    		setInterval(function(){this.speedY=-1;bobby.move();this.speedY=0;},250);
+    	}
+    }
+    //update & draw functions
+	this.update = function(){
+        ctx = myGameArea.context;
+        ctx.fillStyle = color;
+        ctx.fillRect((this.x-1)*(this.width-1), (this.y-1)*(this.height-1), (myGameArea.canvas.width/this.width), (myGameArea.canvas.height/this.height));
+    }
+    this.move = function(){
+    	this.x+=this.speedX;
+    	this.y+=this.speedY;
+    }
+}
+
+
+//stores variables and functions for canvas
+var myGameArea = {
+	canvas: document.createElement("canvas"),
+	start: function() {
+		this.canvas.width = 600;
+		this.canvas.height = 600;
+		this.context = this.canvas.getContext("2d");
+		document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+		this.interval = setInterval(updateGameArea, 20);
+	},
+	clear: function() {
+		this.context.clearRect(0,0,this.canvas.width,this.canvas.height)
+	}
+}
+
+
+//draws to canvas
+function updateGameArea() {
+	myGameArea.clear();
+	tile(tiles,tiles);
+	bobby.update();
+}
